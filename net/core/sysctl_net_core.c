27008b98ac5905213ddef0c02fe43cfa156a986f
Hans Westgaard Ry <hans.westgaard.ry@oracle.com>
2016-02-03 09:26:57 +0100

    net:Add sysctl_max_skb_frags
    [ Upstream commit 5f74f82ea34c0da80ea0b49192bb5ea06e063593 ]
    
    Devices may have limits on the number of fragments in an skb they support.
    Current codebase uses a constant as maximum for number of fragments one
    skb can hold and use.
    When enabling scatter/gather and running traffic with many small messages
    the codebase uses the maximum number of fragments and may thereby violate
    the max for certain devices.
    The patch introduces a global variable as max number of fragments.
    
    Signed-off-by: Hans Westgaard Ry <hans.westgaard.ry@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- net/core/sysctl_net_core.c
+++ net/core/sysctl_net_core.c
@@ -445,6 +445,15 @@ static struct ctl_table net_core_table[] = {
 #endif /* CONFIG_HW_CROSSLAYER_OPT_DBG_MODULE */
 	/*lint -restore */
 #endif /* CONFIG_HW_CROSSLAYER_OPT */
+	{
+		.procname	= "max_skb_frags",
+		.data		= &sysctl_max_skb_frags,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &one,
+		.extra2		= &max_skb_frags,
+	},
 	{ }
 };
 
